587b2d5f15e52202d53021db4e873b70ea13d391
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-10-04 21:35:12 -0300

    OK

--- FrontEnd/src/pages/cadastro/index.jsx
+++ FrontEnd/src/pages/cadastro/index.jsx
@@ -1,6 +1,10 @@
 import './index.scss';
 import Cabecalho from '../../components/cabecalho';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+
+
+import LoadingBar from 'react-top-loading-bar'
+
 
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -13,9 +17,10 @@ export default function Cadastro() {
     const [ senha, setSenha ] = useState('');
 
     const navigate = useNavigate();
+    const ref = useRef();
 
     async function Logar() {
-
+        ref.current.continuousStart()
         try {
            const resp = await axios.post('http://localhost:5000/usuario/login', {
                 nome: nome,
@@ -23,9 +28,10 @@ export default function Cadastro() {
                 senha: senha
             });
 
-            if(resp.status === 200) {
+            setTimeout(() => {
                 navigate('/');
-            }
+            }, 1900)
+
 
         } catch (error) {
             alert(error.response.data.erro);
@@ -36,6 +42,7 @@ export default function Cadastro() {
     return (
 
         <div className='pagina-cadastro'>
+            <LoadingBar color='#f11946' ref={ref} />
             <Cabecalho/>
 
             <article className='titulo'>
